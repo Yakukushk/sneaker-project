bc8bb8827b4ff194bffb9efe62bc623a14d54990
Yakukushk <75535844+Yakukushk@users.noreply.github.com>
2024-05-22 12:11:31 +0200

    Update firebase.js

--- src/connection/firebase.js
+++ src/connection/firebase.js
@@ -7,7 +7,12 @@ import { getAuth } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-// here should be your firebase api connection
+    apiKey: "AIzaSyCHwJVepnmGgm5GcC520_fbiT-v2cFvn0U",
+    authDomain: "freelance-project-e4426.firebaseapp.com",
+    projectId: "freelance-project-e4426",
+    storageBucket: "freelance-project-e4426.appspot.com",
+    messagingSenderId: "195189645192",
+    appId: "1:195189645192:web:30f6cbc53d31fa7d3ce43d"
 };
 
 // Initialize Firebase
